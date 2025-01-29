8d38c56b182c5da06ee338e07c18b931ae5ad165
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-01-29 20:52:24 -0300

    Fix warning in tests

---
+++ jest.config.mjs
@@ -0,0 +1,20 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { readFileSync } from 'fs';
+
+const { compilerOptions } = JSON.parse(readFileSync(new URL('./tsconfig.json', import.meta.url), 'utf8'));
+
+export default {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.(ts|html)$': ['jest-preset-angular', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/' }),
+};
