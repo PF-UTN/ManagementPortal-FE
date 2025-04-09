5923898d4bfb43d4af9eb70865e0bc2f5806a7d1
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-04-08 22:33:55 -0300

    Remove template Component from Common-UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonUiComponent } from '@common-ui';
 import { RegistrationRequestListComponent } from './components/registration-request-list/registration-request-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonUiComponent, RegistrationRequestListComponent],
+  imports: [RouterOutlet, RegistrationRequestListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
