0c52c51a0e22f51db765bad05416caa7b2cc1b09
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-04-08 22:37:58 -0300

    Extract strings to constants

--- projects/common-ui/src/lib/models/column-types.model.ts
+++ projects/common-ui/src/lib/models/column-types.model.ts
@@ -1,1 +1,3 @@
-export type ColumnType = 'value' | 'multiValue' | 'action' | 'tag';
+import { ColumnTypeEnum } from "../constants/column-types.constant";
+
+export type ColumnType = ColumnTypeEnum.VALUE | ColumnTypeEnum.MULTI_VALUE | ColumnTypeEnum.ACTIONS | ColumnTypeEnum.TAG;
--- projects/common-ui/src/public-api.ts
+++ projects/common-ui/src/public-api.ts
@@ -2,8 +2,7 @@
  * Public API Surface of common-ui
  */
 
-export * from './lib/common-ui.service';
-export * from './lib/common-ui.component';
-export * from './lib/components/table/table.component';
-export * from './lib/models/index';
+export * from './lib/components';
+export * from './lib/models';
+export * from './lib/constants';
 
