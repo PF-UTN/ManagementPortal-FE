4c13b0c60637e7be46d2967f6c21b5636d9ad9a4
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-04-24 22:56:04 -0300

    Fix eslint problem

--- src/app/components/approve/approve.component.ts
+++ src/app/components/approve/approve.component.ts
@@ -1,42 +1,45 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 import { RegistrationRequestListItem } from '../../models/registration-request-item.model';
 import { RegistrationRequestService } from '../../services/registration-request.service';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-approve-drawer',
   templateUrl: './approve.component.html',
   styleUrls: ['./approve.component.scss'],
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule]
+  imports: [MatSidenavModule, MatButtonModule],
 })
 export class ApproveDrawerComponent {
-  @Input() data: RegistrationRequestListItem; 
+  @Input() data: RegistrationRequestListItem;
   @Output() close = new EventEmitter<void>();
-  @Output() approveRequest = new EventEmitter<void>(); 
+  @Output() approveRequest = new EventEmitter<void>();
 
   isLoading: boolean = false;
 
   constructor(private registrationRequestService: RegistrationRequestService) {}
 
   closeDrawer(): void {
-    this.close.emit(); 
+    this.close.emit();
   }
 
   approve(): void {
     this.isLoading = true;
-    this.registrationRequestService.approveRegistrationRequest(this.data.id, "").subscribe({
-      next: () => {
-        console.log('Solicitud aprobada con éxito.');
-        this.isLoading = false;
-        this.approveRequest.emit();
-        this.closeDrawer();
-      },
-      error: (err) => {
-        console.error('Error al aprobar la solicitud:', err);
-        this.isLoading = false;
-      }
-    });
+    this.registrationRequestService
+      .approveRegistrationRequest(this.data.id, '')
+      .subscribe({
+        next: () => {
+          console.log('Solicitud aprobada con éxito.');
+          this.isLoading = false;
+          this.approveRequest.emit();
+          this.closeDrawer();
+        },
+        error: (err) => {
+          console.error('Error al aprobar la solicitud:', err);
+          this.isLoading = false;
+        },
+      });
   }
 }
