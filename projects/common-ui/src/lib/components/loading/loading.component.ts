2020437006f7b9f4e677a82bd20a80792993787d
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-06-17 18:38:53 -0300

    Extract to different files loading component

--- projects/common-ui/src/lib/components/loading/loading.component.ts
+++ projects/common-ui/src/lib/components/loading/loading.component.ts
@@ -6,20 +6,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   selector: 'mp-loading',
   standalone: true,
   imports: [CommonModule, MatProgressSpinnerModule],
-  template: `
-    <div class="loading-spinner">
-      <mat-progress-spinner mode="indeterminate" color="primary">
-      </mat-progress-spinner>
-    </div>
-  `,
-  styles: `
-    .loading-spinner {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      height: 100%;
-    }
-  `,
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss'],
 })
 export class LoadingComponent {}
