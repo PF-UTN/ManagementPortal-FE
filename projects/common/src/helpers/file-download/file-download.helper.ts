b2a635db027a7a5f390eb643aca671c787cc9fe0
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-07-28 00:02:44 -0300

    Create helper methods

---
+++ projects/common/src/helpers/file-download/file-download.helper.ts
@@ -0,0 +1,28 @@
+import { HttpResponse } from '@angular/common/http';
+
+export function downloadFileFromResponse(
+  response: HttpResponse<unknown>,
+  defaultFileName: string = 'download.file',
+) {
+  const blob = response.body as Blob;
+
+  let fileName = defaultFileName;
+  const contentDisposition = response.headers?.get('Content-Disposition');
+  if (contentDisposition) {
+    const match = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(
+      contentDisposition,
+    );
+    if (match && match[1]) {
+      fileName = decodeURIComponent(match[1].replace(/['"]/g, '')); // ✅ decode in case it's URL encoded
+    }
+  }
+
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  window.URL.revokeObjectURL(url);
+}
