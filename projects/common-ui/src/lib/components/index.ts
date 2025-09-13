0291b0e95f79a98c28214adfd56c91dc2aac8ea5
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-09-13 13:22:56 -0300

    Remove max-with for ellipsis

--- projects/common-ui/src/lib/components/index.ts
+++ projects/common-ui/src/lib/components/index.ts
@@ -1,5 +1,6 @@
 export * from './button/button.component';
 export * from './button/constants/button-types';
+export * from './ellipsis-text/ellipsis-text.component';
 export * from './input/input.component';
 export * from './lateral-drawer';
 export * from './loading/loading.component';
