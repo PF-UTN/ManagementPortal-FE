0c5fdadd12914aa7667b2ac5acced9ff09d7b5c2
Matias Josue Mayor <81585058+Matimayor@users.noreply.github.com>
2025-10-12 00:12:23 -0300

    Add looker in home page

--- projects/home/src/lib/components/home/home.component.spec.ts
+++ projects/home/src/lib/components/home/home.component.spec.ts
@@ -19,4 +19,18 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the Looker Studio iframe', () => {
+    // Arrange
+
+    // Act
+    const iframe: HTMLIFrameElement | null =
+      fixture.nativeElement.querySelector('iframe');
+
+    // Assert
+    expect(iframe).not.toBeNull();
+    expect(iframe?.src).toContain(
+      'https://lookerstudio.google.com/embed/reporting/7e884610-5c4b-465c-82c4-5b4c2eea7dd0/page/IphaF',
+    );
+  });
 });
