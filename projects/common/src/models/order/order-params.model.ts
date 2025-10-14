a27b98bf0f3ab19ba6ff9fc8ec5211ff7c9c4a6f
Matias Josue Mayor <81585058+Matimayor@users.noreply.github.com>
2025-10-14 14:11:36 -0300

    Add column and filter deliveryMethod in orders

--- projects/common/src/models/order/order-params.model.ts
+++ projects/common/src/models/order/order-params.model.ts
@@ -24,6 +24,7 @@ export interface OrderParams {
     statusName?: string[];
     fromCreatedAtDate?: string | null;
     toCreatedAtDate?: string | null;
+    deliveryMethod?: string[] | null;
   };
   orderBy: OrderOrderBy;
 }
