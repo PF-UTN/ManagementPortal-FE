630c33ef293d186a39f6cc301e90ab6b2dab557c
Matias Josue Mayor <81585058+Matimayor@users.noreply.github.com>
2025-10-15 14:11:26 -0300

    Fix detaild drawer shipment

--- projects/shipment/src/lib/components/shipment-detail-drawer/shipment-detail-drawer.component.ts
+++ projects/shipment/src/lib/components/shipment-detail-drawer/shipment-detail-drawer.component.ts
@@ -1,18 +1,35 @@
-import { LateralDrawerContainer, LoadingComponent } from '@Common-UI';
+import {
+  LateralDrawerContainer,
+  LoadingComponent,
+  TableColumn,
+  ColumnTypeEnum,
+  TableComponent,
+} from '@Common-UI';
 
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { of } from 'rxjs';
 
-import { ShipmentDetail } from '../../models/shipment-deatil.model';
+import {
+  ShipmentDetail,
+  ShipmentOrder,
+} from '../../models/shipment-deatil.model';
+import { orderStatusOptions } from '../../models/shipment-order-status.model';
 import { statusOptions } from '../../models/shipment-status-option.model';
 import { ShipmentService } from '../../services/shipment.service';
 
 @Component({
   selector: 'mp-shipment-detail-drawer',
   standalone: true,
-  imports: [LoadingComponent, CommonModule, MatIconModule, MatButtonModule],
+  imports: [
+    LoadingComponent,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    TableComponent,
+  ],
   templateUrl: './shipment-detail-drawer.component.html',
   styleUrl: './shipment-detail-drawer.component.scss',
 })
@@ -20,6 +37,24 @@ export class ShipmentDetailDrawerComponent
   extends LateralDrawerContainer
   implements OnInit
 {
+  orderColumns: TableColumn<ShipmentOrder>[] = [
+    {
+      columnDef: 'id',
+      header: 'N° Orden',
+      type: ColumnTypeEnum.VALUE,
+      width: '60%',
+      value: (order) => order.id.toString(),
+    },
+    {
+      columnDef: 'status',
+      header: 'Estado',
+      type: ColumnTypeEnum.VALUE,
+      width: '40%',
+      align: 'left',
+      value: (order) => this.getOrderStatusLabel(order.status),
+    },
+  ];
+
   shipmentId!: number;
   isLoading = signal(true);
   data = signal<ShipmentDetail | null>(null);
@@ -31,6 +66,16 @@ export class ShipmentDetailDrawerComponent
   getStatusLabel(status: string): string {
     const found = statusOptions.find((opt) => opt.key === status);
     return found ? found.value : status;
+  }
+
+  getOrderStatusLabel(status: string): string {
+    const found = orderStatusOptions.find((opt) => opt.key === status);
+    return found ? found.value : status;
+  }
+
+  get orderDataSource$() {
+    const orders = this.data()?.orders ?? [];
+    return of(orders);
   }
 
   ngOnInit(): void {
