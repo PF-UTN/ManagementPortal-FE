dd45df2973c7d6abc001ae563d33b48bc17788a0
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-10-24 20:50:31 -0300

    Fix wrong apiBaseUrl

--- projects/common/src/environments/environment.ts
+++ projects/common/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'https://dev-management-portal-be.vercel.app',
   cdnBaseUrl: 'https://rykocsrdf1gsyk57.public.blob.vercel-storage.com',
   mercadoPagoPublicKey: 'APP_USR-62844de7-2def-4246-8b33-357f1f801840',
   lookerUrl:
