3dc0d47ea69b69df362060748b935a9ceb942c10
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2025-10-27 00:11:09 -0300

    Fix: Notifications

--- projects/home/src/lib/components/home/home.component.spec.ts
+++ projects/home/src/lib/components/home/home.component.spec.ts
@@ -1,28 +1,47 @@
 import { environment } from '@Common';
+import { NotificationService } from '@Notification';
 
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let notificationService: NotificationService;
 
   beforeEach(async () => {
+    const notificationServiceMock = {
+      getNotifications: jest.fn().mockReturnValue(of([])),
+    } as unknown as jest.Mocked<NotificationService>;
+
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        {
+          provide: NotificationService,
+          useValue: notificationServiceMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
+    notificationService = TestBed.inject(NotificationService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call NotificationService.getNotifications on init', () => {
+    expect(notificationService.getNotifications).toHaveBeenCalledTimes(1);
   });
 
   it('should render the Looker Studio iframe', () => {
