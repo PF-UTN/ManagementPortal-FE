134376a3ec17c92465ad2e918433b0be4bfe7597
Martin Lambrecht <martinoscarlambrecht@gmail.com>
2025-10-30 00:37:24 -0300

    Change to test env mercadoPago on prod

--- projects/common/src/environments/environment.production.ts
+++ projects/common/src/environments/environment.production.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://management-portal-be.vercel.app',
   cdnBaseUrl: 'https://uztxlye8x6st5gv0.public.blob.vercel-storage.com',
-  mercadoPagoPublicKey: 'APP_USR-cbf92ac5-4950-4fff-9658-8247de3a5d0a',
+  mercadoPagoPublicKey: 'APP_USR-62844de7-2def-4246-8b33-357f1f801840',
+  // 'APP_USR-cbf92ac5-4950-4fff-9658-8247de3a5d0a',
   lookerUrl:
     'https://lookerstudio.google.com/embed/reporting/262800d4-77d0-4584-955c-b9a8c997f976/page/lL6bF',
 };
